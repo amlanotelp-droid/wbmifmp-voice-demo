3579dbbdd34803eda1e708fe22b3d2df82163338
Amlan Sarkar <amlan.otelp@gmail.com>
2026-02-26 14:34:27 +0530

    Fix RetrievalQA import

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_community.vectorstores import Chroma
 from langchain_openai import ChatOpenAI
-from langchain.chains import RetrievalQA
+from langchain.chains.retrieval_qa.base import RetrievalQA
 from langchain.prompts import PromptTemplate
 
 st.set_page_config(page_title="WBMIFMP Voice AI Demo")
