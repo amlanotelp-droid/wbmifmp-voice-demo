49827abe7262827872369c31b67f43f9aed584de
Amlan Sarkar <amlan.otelp@gmail.com>
2026-02-26 14:05:04 +0530

    Fix OpenAI embeddings import

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import requests
 import streamlit as st
 from bs4 import BeautifulSoup
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings
+from langchain_openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import RetrievalQA
